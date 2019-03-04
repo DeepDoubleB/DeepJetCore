cd722c7341ed2a2144d8265712b434db6b5390b3
Javier Duarte <jduarte1@fnal.gov>
2019-03-04 15:25:38 -0800

    used version

--- bin/convertToTF.py
+++ bin/convertToTF.py
@@ -9,11 +9,13 @@ except ImportError:
             
             
 from keras.models import load_model
+from keras.layers import Input
+from testing import testDescriptor
 from argparse import ArgumentParser
 from keras import backend as K
 from Losses import * #needed!
 import os
-
+import tensorflow as tf
 
 parser = ArgumentParser('')
 parser.add_argument('inputModel')
@@ -25,14 +27,56 @@ if os.path.isdir(args.outputDir):
     raise Exception('output directory must not exists yet')
 
 
-model=load_model(args.inputModel, custom_objects=global_loss_list)
-import tensorflow as tf
-import keras.backend as K
+from DeepJet_models_final import conv_model_final
+model = conv_model_final([Input(shape=(1,27)),Input(shape=(60,8)),Input(shape=(5,2))], 2, 0, ['db','cpf','sv'])
+model.load_weights(args.inputModel)
+#model=load_model(args.inputModel, custom_objects=global_loss_list)
+print(model.summary())
+
+
+num_output = 2
+
+
+#pred = [None]*num_output
+#pred_node_names = [None]*num_output
+pred_node_names = ['ID_pred/Softmax']
+#pred_node_names = ['softmax/Softmax']
+#prefix_output_node_names_of_final_network = 'output_node'
+#output_graph_name = 'constant_graph_weights.pb'
+#for i in range(num_output):
+#    pred_node_names[i] = prefix_output_node_names_of_final_network+str(i)
+#    pred[i] = tf.identity(model.output[i], name=pred_node_names[i])
+#print('output nodes names are: ', pred_node_names)
+
+K.set_learning_phase(0)
 tfsession=K.get_session()
 saver = tf.train.Saver()
-tfoutpath=args.outputDir+'/tf'
-import os
-os.system('mkdir -p '+tfoutpath)
+tfoutpath=args.outputDir+'/tf.checkpoint'
+
+
+#os.system('mkdir -p '+tfoutpath)
+
+
+from tensorflow.python.framework import graph_util
+from tensorflow.python.framework import graph_io
+#nonconstant_graph = tfsession.graph.as_graph_def()
+constant_graph = graph_util.convert_variables_to_constants(tfsession, tfsession.graph.as_graph_def(), pred_node_names)
+
+f = 'constantgraph.pb.ascii'
+tf.train.write_graph(constant_graph, args.outputDir, f, as_text=True)
+print('saved the graph definition in ascii format at: ', os.path.join(args.outputDir, f))
+
+f = 'constantgraph.pb'
+tf.train.write_graph(constant_graph, args.outputDir, f, as_text=False)
+print('saved the graph definition in pb format at: ', os.path.join(args.outputDir, f))
+
+
+#graph_io.write_graph(constant_graph, args.outputDir, output_graph_name, as_text=False)
+#print('saved the constant graph (ready for inference) at: ', os.path.join(args.outputDir, output_graph_name))
+
 saver.save(tfsession, tfoutpath)
+
+
+
 
 
